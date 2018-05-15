fd5e635bde206f5dc23876c449ab2abbbc15cef3
Thanzeel <tnlthanzeel@gmail.com>
2018-05-15 20:43:15 +0530

    init
    initialize java SE project

---
+++ src/glorygame/GloryGame.java
@@ -0,0 +1,27 @@
+package glorygame;
+
+import java.io.*;
+import java.net.*;
+
+public class GloryGame {
+
+    static Socket socket;
+    static DataInputStream in;
+    
+    public static void main(String[] args) {
+        try{
+            System.out.println("Connecting...");
+            socket=new Socket("localhost",7777);
+            System.out.println("connection successfull");
+            in =new DataInputStream(socket.getInputStream());
+            System.out.println("Receiving information");
+            String test=in.readUTF();
+            System.out.println("Message from sever: "+test);
+            
+        }
+        
+        catch(Exception e )
+        {}
+    }
+    
+}
