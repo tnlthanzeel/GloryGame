158d20082a96c8beebc73735ed00fd8e42908040
Akmal <mohamedakmal03@gmail.com>
2018-05-17 20:19:58 +0530

    glory element properties added

--- src/Glory_Schema/GloryElement.java
+++ src/Glory_Schema/GloryElement.java
@@ -6,7 +6,10 @@ package Glory_Schema;
  * @author Thanzeel
  */
 public class GloryElement {
-    
+    int userId;
+    static int gameHighScore;
+    int numberOfPlayers;
+    String playerName;
 }
 
 
