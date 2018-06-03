b5fd494f8858d4b8dce685f8e50a1173b0814b6d
tnlthanzeel <tnlthanzeel@gmail.com>
2018-06-03 21:37:02 +0530

    stripp comments

--- src/Glory_Schema/DBConnection.java
+++ src/Glory_Schema/DBConnection.java
@@ -12,12 +12,11 @@ public class DBConnection {
     public static Connection GetConnection() {
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            //String url = "jdbc:mysql://sql12.freemysqlhosting.net:3307/sql12241164";
             String url = "jdbc:mysql://db4free.net:3306/glorygame?autoReconnect=true&useSSL=false";
-
-            //conObj = DriverManager.getConnection(url, "sql12241164", "JI6BCHXkTh");
             conObj = DriverManager.getConnection(url, "glorygamedb", "553e27ec");
 
+            //conObj = DriverManager.getConnection(url, "sql12241164", "JI6BCHXkTh");
+            //String url = "jdbc:mysql://sql12.freemysqlhosting.net:3307/sql12241164";
             return conObj;
         } catch (Exception ex) {
             ex.printStackTrace();
