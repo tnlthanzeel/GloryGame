aedea6c740a412ad17969a2195aee944dccb3a2c
tnlthanzeel <tnlthanzeel@gmail.com>
2018-06-10 21:09:40 +0530

    generate first three letters done

--- src/Glory_Schema/LetterValueElement.java
+++ src/Glory_Schema/LetterValueElement.java
@@ -1,9 +1,22 @@
 package Glory_Schema;
 
+import java.util.Random;
+
 /**
  *
  * @author Thanzeel
  */
-public class LetterValueElement extends GloryElement{
+public class LetterValueElement extends GloryElement {
+
     int score;
+    Random random = new Random();
+
+    public char[] generateFirstThreeeLetters() {
+        char[] firstThreeLetters = new char[3];
+
+        for (int i = 0; i < 3; i++) {
+            firstThreeLetters[i] = (char) (random.nextInt(26) + 'a');
+        }
+        return firstThreeLetters;
+    }
 }
