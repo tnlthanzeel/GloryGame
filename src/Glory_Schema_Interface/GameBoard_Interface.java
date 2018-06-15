5e13b01bce0838839adbc57452b829547a7dfaa3
PavithraJayathilake <39290772+PavithraJayathilake@users.noreply.github.com>
2018-06-15 20:09:43 +0530

    online player interface and eit button in gameboard

--- src/Glory_Schema_Interface/GameBoard_Interface.java
+++ src/Glory_Schema_Interface/GameBoard_Interface.java
@@ -53,6 +53,7 @@ public class GameBoard_Interface extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        btn_exit = new javax.swing.JButton();
 
         jButton1.setText("jButton1");
 
@@ -132,6 +133,16 @@ public class GameBoard_Interface extends javax.swing.JFrame {
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Name");
 
+        btn_exit.setBackground(new java.awt.Color(255, 255, 255));
+        btn_exit.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        btn_exit.setForeground(new java.awt.Color(255, 0, 0));
+        btn_exit.setText("Exit");
+        btn_exit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_exitActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -170,7 +181,8 @@ public class GameBoard_Interface extends javax.swing.JFrame {
                         .addComponent(txt1, javax.swing.GroupLayout.PREFERRED_SIZE, 1071, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btn_exit)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel3)
                 .addGap(41, 41, 41)
                 .addComponent(jLabel2)
@@ -185,8 +197,10 @@ public class GameBoard_Interface extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2)
-                    .addComponent(jLabel3))
-                .addGap(66, 66, 66)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel3)
+                        .addComponent(btn_exit)))
+                .addGap(61, 61, 61)
                 .addComponent(jLabel1)
                 .addGap(28, 28, 28)
                 .addComponent(btn1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -249,6 +263,11 @@ public class GameBoard_Interface extends javax.swing.JFrame {
         btn2.setText(String.valueOf(firsthree[1]));
     }//GEN-LAST:event_btn2ActionPerformed
 
+    private void btn_exitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_exitActionPerformed
+        // TODO add your handling code here:
+      
+    }//GEN-LAST:event_btn_exitActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -299,6 +318,7 @@ public class GameBoard_Interface extends javax.swing.JFrame {
     private javax.swing.JButton btn7;
     private javax.swing.JButton btn8;
     private javax.swing.JButton btn9;
+    private javax.swing.JButton btn_exit;
     private javax.swing.JButton btn_submit;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
