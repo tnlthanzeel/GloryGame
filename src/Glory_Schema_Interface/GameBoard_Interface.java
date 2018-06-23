1992e1abcde8dadb8e0fd7c00cd4deed9af42153
tnlthanzeel <tnlthanzeel@gmail.com>
2018-06-24 00:09:35 +0530

    timer added

--- src/Glory_Schema_Interface/GameBoard_Interface.java
+++ src/Glory_Schema_Interface/GameBoard_Interface.java
@@ -9,6 +9,8 @@ import Glory_Schema.LetterValueElement;
 import Glory_Schema.ScoreElement;
 import Glory_Schema.WordElement;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -22,11 +24,13 @@ public class GameBoard_Interface extends javax.swing.JFrame {
     public char[] firsthree = new char[3];
     WordElement wordelement;
     ScoreElement scoreElement;
+    int totalMarks=0;
     /**
      * Creates new form GameBoard_Interface
      */
     public GameBoard_Interface()  {
         initComponents();
+        setTime();
         setExtendedState(MAXIMIZED_BOTH);
         letterElement = new LetterValueElement();
         firsthree = letterElement.generateFirstThreeeLetters();
@@ -45,6 +49,7 @@ public class GameBoard_Interface extends javax.swing.JFrame {
 
         jButton1 = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
         btn1 = new javax.swing.JButton();
         btn2 = new javax.swing.JButton();
         btn3 = new javax.swing.JButton();
@@ -62,12 +67,16 @@ public class GameBoard_Interface extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         btn_exit = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
 
         jButton1.setText("jButton1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(0, 102, 102));
+
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("0");
 
         btn1.setBackground(new java.awt.Color(102, 255, 102));
         btn1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
@@ -201,6 +210,8 @@ public class GameBoard_Interface extends javax.swing.JFrame {
             }
         });
 
+        jLabel5.setText("jLabel5");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -240,26 +251,32 @@ public class GameBoard_Interface extends javax.swing.JFrame {
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addComponent(btn_exit)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(243, 243, 243)
                 .addComponent(jLabel3)
-                .addGap(41, 41, 41)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel2)
+                .addGap(77, 77, 77)
+                .addComponent(jLabel4)
                 .addContainerGap())
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(488, 488, 488)
                 .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(btn_exit)
                     .addComponent(jLabel2)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jLabel3)
-                        .addComponent(btn_exit)))
+                    .addComponent(jLabel4))
                 .addGap(61, 61, 61)
-                .addComponent(jLabel1)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(28, 28, 28)
                 .addComponent(btn1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -385,6 +402,8 @@ boolean result=wordelement.contains(word);
 
 if(result){
     int score=ScoreElement.calculateScore();
+    totalMarks+=score;
+    jLabel4.setText(String.valueOf(totalMarks));
     System.out.println(score);
     JOptionPane.showMessageDialog(null, "Correct Word", "Correct", JOptionPane.INFORMATION_MESSAGE);
 }
@@ -452,7 +471,37 @@ else
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    public static javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField txt1;
     // End of variables declaration//GEN-END:variables
+
+  private void setTime()
+
+    {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("H:mm:ss");
+
+        new Thread(new Runnable()
+        {
+
+            @Override
+            public void run()
+            {
+                while (true)
+                {
+
+                    Date d = new Date();
+                    jLabel5.setText(sdf.format(d));
+
+                }
+            }
+        }).start();
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+
+
 }
